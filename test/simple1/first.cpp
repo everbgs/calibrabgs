03c6d9fa5a43778bc52120711ff1e4359866a608
everbgs <evertonb@unochapeco.edu.br>
2014-01-03 22:29:16 -0200

    testando linux 1

---
+++ test/simple1/first.cpp
@@ -0,0 +1,34 @@
+/********************************************************************************
+* 2014, Everton Borges <evertonb@unochapeco.edu.br>
+*
+* Teste simples para verificação da instalação da biblioteca OpenCV no Linux
+*
+**********************************************************************************/
+
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+int main(int argc, char** argv)
+{
+ 
+	//Carregar a imagem
+	IplImage *img = cvLoadImage (argv[1], -1);
+	
+	//Cria uma janela para exibir a imagem
+   	cvNamedWindow(argv [1], CV_WINDOW_AUTOSIZE);
+	//Exibir a imagem
+   	cvShowImage(argv [1], img);
+
+	// Espera até fechar a janela
+   	cvWaitKey(0);
+	
+	// Libera o objeto imagem
+   	cvReleaseImage(& img);	
+	
+	// Destruir a janela
+   	cvDestroyWindow(argv [1]);
+	
+
+	return 0;
+}
