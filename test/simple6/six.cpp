4fe92257388f4824b242300a6c7b9663154d2e0c
Everton Borges <evertonb@unochapeco.edu.br>
2014-05-07 01:44:03 -0300

    implementação das funçoes principais

--- test/simple6/six.cpp
+++ test/simple6/six.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
+#include <time.h> 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -11,35 +12,96 @@ using namespace cv;
 class Filmadora {
 
 private:
+	/*Classe responsavel pela captura*/
 	VideoCapture camera;
-	int disp;
-	string camDisp;
+
+	/*Tipos de dispositivos, parametros:
+		inteiro = Geralmente a WebCam ou cameras acopladas a maquina
+		string = Cameras externas, Camera IP
+	*/
+	int intDisp;
+	string strDisp;
+
+	/*Atributos para mostrar o tempo*/
+	char tempo_str[30];
+  	time_t rawtime;
+	struct tm * timeinfo;
+
+	/*Abre Camera para captura*/
 	void abrirCamera(void);
+
+	/*Retorna dia/Mes/Ano Hora:Minuto:Segundo*/
+	char* getDateTime(void);
+
+	/*Retorna verdadeiro caso haja diferença entre as duas imagens*/
+	bool isDetectouMovimento(Mat& m1, Mat& m2);
 public:
-	Filmadora(): disp(-2), camDisp(""){};
+	/*Informa o Dispositivo que vai ser utilizado*/
 	void setDispositivo(int d);
 	void setDispositivo(string s);
-	void gravarVideoAVI(void);
-	void gravarVideoMovimento(void);
 
+	/*Grava video da camera*/
+	void gravarVideoCamera(string nomeArq);
+	void gravarVideoCameraMovimento(string nomeArq);
+
+	/*Função extra, grava somente o movimento de um arquivo de video*/
+	void gravarVideoMovimentoArquivo(string nomeArq, string nameSave);
 };
-
-void Filmadora::setDispositivo(int d){this->disp = d;}
-
-void Filmadora::setDispositivo(string s){this->camDisp = s;}
 
 void Filmadora::abrirCamera(void)
 {
-	if (this->camDisp != "")
-		this->camera.open(this->camDisp);
-	else if (disp > -2)
-		this->camera.open(this->disp);
+	if (this->camera.isOpened())
+		this->camera.release();	
+
+	if (this->strDisp != "")
+		this->camera.open(this->strDisp);
+	else if (intDisp > -2)
+		this->camera.open(this->intDisp);
 }
 
-void Filmadora::gravarVideoAVI(void) 
+char* Filmadora::getDateTime(void)
 {
-	this->camera.release();
-	this->abrirCamera();	
+	time(&rawtime);  	
+	timeinfo = localtime(&rawtime);
+	strftime(tempo_str, 30, "%d/%m/%G %T", timeinfo);
+	return tempo_str;
+}
+
+bool Filmadora::isDetectouMovimento(Mat& m1, Mat& m2)
+{
+	int nr = m1.rows;
+	int nc = m1.cols;
+	double val;
+
+	for (int i=0; i<nr; i++)
+	{
+		for (int l=0; l<nc; l++)		
+		{
+			val = (abs(m1.at<cv::Vec3b>(i,l)[0] - m2.at<cv::Vec3b>(i,l)[0]) +
+  				   abs(m1.at<cv::Vec3b>(i,l)[1] - m2.at<cv::Vec3b>(i,l)[1]) +
+				   abs(m1.at<cv::Vec3b>(i,l)[2] - m2.at<cv::Vec3b>(i,l)[2])) / 3;
+			if (val > 35)
+				return true;
+		}
+	}
+	return false;
+}
+
+void Filmadora::setDispositivo(int d)
+{
+	this->strDisp = "";
+	this->intDisp = d;
+}
+
+void Filmadora::setDispositivo(string s)
+{
+	this->strDisp = s;
+	this->intDisp = -2;
+}
+
+void Filmadora::gravarVideoCamera(string nomeArq)
+{
+	this->abrirCamera();
 
 	if (!this->camera.isOpened())
 		throw "Erro ao abrir camera para captura";
@@ -47,114 +109,135 @@ void Filmadora::gravarVideoAVI(void)
 	double width = this->camera.get(CV_CAP_PROP_FRAME_WIDTH);
     double height = this->camera.get(CV_CAP_PROP_FRAME_HEIGHT); 
     Mat frame; 
-
+	
 	printf("Tamanho da Captura\n%.0f x %.0f\n", width, height); 
     namedWindow ("Camera", CV_WINDOW_AUTOSIZE);     
 
-	VideoWriter video("video.avi", CV_FOURCC('D','I','V','3'), 20, cvSize((int)width, (int)height));
+	nomeArq += ".avi";
+	VideoWriter video(nomeArq, CV_FOURCC('D','I','V','3'), 20, cvSize((int)width, (int)height));
     if(!video.isOpened())
-		throw "Não foi possivel criar o video capturado";
+		throw "Não foi possivel criar o arquivo de video capturado";
  
-	puts("Press Esc to stop recording."); 	
-    while(true)
-    {
-        this->camera >> frame; 		
-		
-        if(frame.empty())
-			throw "Erro ao capturar frame";
-
-        video << frame;
-
+	char tecla;
+	puts("Pressione esc para sair."); 	
+    while(this->camera.read(frame))
+    {		
+		video << frame;
+		putText(frame, this->getDateTime(), Point(1, 40), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255,255,255));
         imshow("Camera", frame);
 
-		char c = waitKey(20); 
-        if(c == 27) break;
+		tecla = waitKey(20); 
+        if (tecla == 27) break;
     }
 	destroyWindow("Camera");
+	this->camera.release();
 }
 
 
-void Filmadora::gravarVideoMovimento(void)
+void Filmadora::gravarVideoCameraMovimento(string nomeArq)
 {
-	this->camera.release();
 	this->abrirCamera();	
 
 	if (!this->camera.isOpened())
 		throw "Erro ao abrir camera para captura";
-	
-    namedWindow ("Camera", CV_WINDOW_AUTOSIZE);         
-    //namedWindow ("Camera2", CV_WINDOW_AUTOSIZE);         
-
-	Mat frame, frameAnt; 	
-	CvPoint minVal, maxVal;
 
 	double width = this->camera.get(CV_CAP_PROP_FRAME_WIDTH);
     double height = this->camera.get(CV_CAP_PROP_FRAME_HEIGHT);
-	VideoWriter video("mov.avi", CV_FOURCC('D','I','V','3'), 20, cvSize((int)width, (int)height));
+
+	nomeArq += ".avi";
+	VideoWriter video(nomeArq, CV_FOURCC('D','I','V','3'), 20, cvSize((int)width, (int)height));
+
     if(!video.isOpened())
 		throw "Não foi possivel criar o video capturado";
 
-	
+	Mat frame, frameAnt; 	
+	bool achou;	
+	char teclado;
+
 	this->camera >> frameAnt; 			
-	if (frameAnt.empty()) throw "Erro ao capturar frame";
+	if (!this->camera.read(frameAnt))
+		throw "Erro ao capturar frame inicial";
+
+	blur(frameAnt, frameAnt, cv::Size(3,3));
 	waitKey(20); 
-
-	double val;
-
-	while(true)
+	
+    namedWindow ("Camera", CV_WINDOW_AUTOSIZE);     
+	puts("Pressione esc para sair.");     
+	while(this->camera.read(frame))
     {
-        this->camera >> frame; 		
-		
-        if(frame.empty()) throw "Erro ao capturar frame";  
-
 		blur(frame, frame, cv::Size(3,3));
-
-		minVal.x = frame.size().width;
-        minVal.y = frame.size().height;
-        maxVal.x = 0;
-        maxVal.y = 0;
-
-		for (int i=0; i<frame.rows; i++)
-		{
-			for (int l=0; l<frame.cols; l++)		
-			{
-				val = (abs(frame.at<cv::Vec3b>(i,l)[0] - frameAnt.at<cv::Vec3b>(i,l)[0]) +
-					  abs(frame.at<cv::Vec3b>(i,l)[1] - frameAnt.at<cv::Vec3b>(i,l)[1]) +
-					  abs(frame.at<cv::Vec3b>(i,l)[2] - frameAnt.at<cv::Vec3b>(i,l)[2])) / 3;
-				if (val > 35)
-				{
-					if(l<minVal.y)
-		                minVal.y = l;
-		            if(i<minVal.x)
-		                minVal.x = i;
-		            if(l>maxVal.y)
-		                maxVal.y = l;
-		            if(i>maxVal.x)
-		                maxVal.x = i;
-				}
-			}
-		}	
+		achou = this->isDetectouMovimento(frame, frameAnt);	
 	
 	    frameAnt = frame.clone();
-		if (maxVal.x || maxVal.y)
+
+		putText(frame, this->getDateTime(), Point(1, 40), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255,255,255));
+		if (achou)
 		{
-			video << frame;
-			rectangle(frame, minVal, maxVal, cv::Scalar(0,0,255), 1, 8, 0);		
+			video << frame;			
+			putText(frame, "Gravando...", Point(1, 20), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255,255,255));
 		}
 	
         imshow("Camera", frame);
-        //imshow("Camera2", frameAnt);
-		char c = waitKey(20); 
-		
-        if(c == 27) break;
+		teclado = waitKey(20); 		
+        if (teclado == 27) break;
     }
 	destroyWindow("Camera");
+	this->camera.release();
+}
+
+void Filmadora::gravarVideoMovimentoArquivo(string nomeArq, string nameSave)
+{
+	nomeArq += ".avi";
+	VideoCapture arquivo(nomeArq);
+
+	if (!arquivo.isOpened())
+		throw "Erro ao abrir arquivo para captura";
+	
+	double width = this->camera.get(CV_CAP_PROP_FRAME_WIDTH);
+    double height = this->camera.get(CV_CAP_PROP_FRAME_HEIGHT);
+	
+	nameSave += ".avi";
+	VideoWriter video(nameSave, CV_FOURCC('D','I','V','3'), 20, cvSize((int)width, (int)height));
+	if (!video.isOpened())
+		throw "Não foi possivel criar o video capturado";
+	
+	Mat frame, frameAnt;	
+	bool achou;
+
+	if (!camera.read(frameAnt))
+		throw "Não foi capturar o frame inicial";		
+
+	blur(frameAnt, frameAnt, cv::Size(3,3));
+	waitKey(30);
+
+	namedWindow("Arquivo");	
+	while(camera.read(frame))
+    {		
+		blur(frame, frame, cv::Size(3,3));
+		achou = this->isDetectouMovimento(frame, frameAnt);	
+	
+	    frameAnt = frame.clone();
+
+		putText(frame, this->getDateTime(), Point(1, 40), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255,255,255));
+		if (achou)
+		{
+			video << frame;			
+			putText(frame, "Gravando...", Point(1, 20), FONT_HERSHEY_COMPLEX_SMALL, 1, Scalar(255,255,255));
+		}
+	
+        imshow("Camera", frame);
+		waitKey(30); 		        
+    }
+	destroyWindow("Camera");
+	this->camera.release();
 }
 
 void cls(void);
 
 int main()
 {
+	/* Testar e terminar menu
+
 	int op;
 	Filmadora film;
 	film.setDispositivo(0);
@@ -169,12 +252,18 @@ int main()
 		scanf("%d", &op);
 		switch(op)
 		{
+			case 1:
+				film.gravarVideoAVI();				
+				break;				
+			case 2:
+				film.exibirVideo();				
+				break;				
 			case 3:
 				film.gravarVideoMovimento();				
 				break;				
 		}
 	//	cls();
-	}while (op != 0);	
+	}while (op != 0);	*/
 
 	return 0;
 }
